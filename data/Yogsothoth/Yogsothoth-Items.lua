050db6d23f702c4f7adbbd649f6def1017c80a54
lwrand <lwrand@users.noreply.github.com>
2018-11-17 20:49:24 +0100

    Add files via upload
    Added: Yogsothoth

---
+++ data/Yogsothoth/Yogsothoth-Items.lua
@@ -0,0 +1,45 @@
+--Augmented items that you'll use among multiple jobs.
+
+gear.gada_enhancing_club = {name="Gada",augments={'Enh. Mag. eff. dur. +6','STR+3','Mag. Acc.+8','"Mag.Atk.Bns."+17','DMG:+7',}}
+gear.gada_healing_club = {name="Gada",augments={'"Cure" potency +9%','Mag. Acc.+11','"Mag.Atk.Bns."+13','DMG:+3',}}
+gear.gada_matk_club = {name="Gada",augments={'Mag. Acc.+23','"Mag.Atk.Bns."+24','DMG:+14',}}
+gear.grioavolr_nuke_staff = {name="Grioavolr",augments={'Magic burst dmg.+8%','MP+61','Mag. Acc.+16','"Mag.Atk.Bns."+30','Magic Damage +7',}}
+gear.grioavolr_fc_staff = {name="Grioavolr",augments={'"Fast Cast"+7','MP+20','Mag. Acc.+17','"Mag.Atk.Bns."+10','Magic Damage +5',}}
+gear.grioavolr_pet_staff = {name="Grioavolr",augments={'Blood Pact Dmg.+8','Pet: STR+10','Pet: Mag. Acc.+19','Pet: "Mag.Atk.Bns."+30',}}
+
+--gear.chironic_enfeeble_hands = {name="Chironic Gloves",augments={'Mag. Acc.+20 "Mag.Atk.Bns."+20','MND+10','Mag. Acc.+15',}}
+
+gear.taeon_Snap_head  = {name="Taeon Chapeau", augments={'"Snapshot"+5','"Snapshot"+5'}}
+
+gear.adhemar_tp_hands = {name="Adhemar Wrist. +1", augments={'AGI+12','Rng.Acc.+20','Rng.Atk.+20',}}
+gear.adhemar_tp_dw_hands = {name="Adhemar Wristbands", augments={'STR+10','DEX+10','Attack+15',}}
+gear.adhemar_tp_legs = {name="Adhemar Kecks", augments={'AGI+10','Rng.Acc.+15','Rng.Atk.+15',}}
+gear.adhemar_tp_feet = {name="Adhe. Gamashes +1", augments={'HP+65','"Store TP"+7','"Snapshot"+10',}}
+
+gear.herculean_th_head = {name="Herculean Helm", augments={'VIT+10','Chance of successful block +2','"Treasure Hunter"+2','Accuracy+1 Attack+1','Mag. Acc.+12 "Mag.Atk.Bns."+12',}}
+gear.herculean_wsd_head = {name="Herculean Helm", augments={'"Mag.Atk.Bns."+18','Weapon skill damage +4%','INT+1','Mag. Acc.+13',}}
+gear.herculean_mwsd_hands = { name="Herculean Gloves", augments={'"Mag.Atk.Bns."+11','Weapon skill damage +4%','INT+9',}}
+gear.herculean_wsd_legs = {name="Herculean Trousers", augments={'Weapon skill damage +5%','Rng.Acc.+13','Rng.Atk.+21',}}
+gear.herculean_mwsd_legs = {name="Herculean Trousers", augments={'"Mag.Atk.Bns."+24','Weapon skill damage +4%',}}
+gear.herculean_wsd_feet = {name="Herculean Boots", augments={'Rng.Atk.+10','Weapon skill damage +3%','AGI+10','Rng.Acc.+9',}}
+gear.herculean_mwsd_feet = {name="Herculean Boots", augments={'MND+2','"Cure" spellcasting time -9%','Weapon skill damage +4%','Mag. Acc.+20 "Mag.Atk.Bns."+20',}}
+
+--gear.merlinic_nuke_head = {name="Merlinic Hood",augments={'Mag. Acc.+21 "Mag.Atk.Bns."+21','INT+15','Mag. Acc.+3','"Mag.Atk.Bns."+13',}}
+
+gear.odyssean_wsd_hands = { name="Odyssean Gauntlets", augments={'Accuracy+5','Weapon skill damage +3%','Attack+15',}}
+gear.odyssean_stp_legs = { name="Odyssean Cuisses", augments={'Accuracy+13 Attack+13','"Dbl.Atk."+4','Accuracy+8',}}
+gear.odyssean_fc_legs = {name="Odyssean Cuisses",augments={'"Fast Cast"+6','"Mag.Atk.Bns."+12',}}
+
+gear.valorous_magical_wsd_head = {name="Valorous Mask",augments={'"Mag.Atk.Bns."+17','Weapon skill damage +5%','Attack+10',}}
+gear.valorous_wsd_head = {name="Valorous Mask",augments={'Attack+23','Weapon skill damage +4%','Accuracy+15',}}
+gear.valorous_wsd_hands = { name="Valorous Mitts", augments={'Attack+30','Weapon skill damage +5%','VIT+1','Accuracy+7',}}
+gear.valorous_wsd_feet = { name="Valorous Greaves", augments={'Weapon skill damage +5%','Accuracy+7','Attack+13',}}
+
+sets.Sheltered = {ring2="Sheltered Ring"}
+sets.Capacity = {back="Mecisto. Mantle"}
+
+sets.buff.Doom = {waist="Gishdubar Sash",ring1="Eshmun's Ring",ring2="Eshmun's Ring"}
+sets.TreasureHunter = {head=gear.herculean_th_head,waist="Chaac Belt"}
+
+sets.precast.Item['Hallowed Water'] = {}
+sets.precast.Item['Holy Water'] = {}
